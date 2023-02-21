021a5aec93fb7265a619451b44265e306b10035e
danmiee <dan-mi@kakao.com>
2023-02-21 18:06:46 +0900

    [230221] STYLE 테이블 생성 관련 변수명 수정

--- predictbit/src/main/java/com/predictbit/persistence/UserRepository.java
+++ predictbit/src/main/java/com/predictbit/persistence/UserRepository.java
@@ -1,7 +1,7 @@
 package com.predictbit.persistence;
 
-import com.predictbit.domain.User;
+import com.predictbit.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<Users, String> {
 }
