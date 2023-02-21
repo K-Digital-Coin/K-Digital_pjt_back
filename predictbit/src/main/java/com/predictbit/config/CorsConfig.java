a554f854ad02c974775e6e65f05d86b97ce314eb
danmiee <dan-mi@kakao.com>
2023-02-21 18:13:59 +0900

    [230221] FEAT CORS 에러방지

---
+++ predictbit/src/main/java/com/predictbit/config/CorsConfig.java
@@ -0,0 +1,15 @@
+package com.predictbit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:3000", "http://10.125.121.170:3000");
+	}
+}
