7315ba014b04afae15d5993c2b0b0eeca4614a18
cyccc95 <cyccc95@naver.com>
2023-03-02 11:57:32 +0900

    [230302] FEAT JWT + WebSocket 완성

--- backend/src/main/java/com/backend/web/indicator/controller/IndicatorController.java
+++ backend/src/main/java/com/backend/web/indicator/controller/IndicatorController.java
@@ -1,9 +1,9 @@
-package com.backend.web.historyCoin.controller;
+package com.backend.web.indicator.controller;
 
 import com.backend.common.model.ApiResponse;
 import com.backend.common.model.CustomException;
 import com.backend.common.util.ResponseMessageUtil;
-import com.backend.web.historyCoin.service.HistoryCoinService;
+import com.backend.web.indicator.service.IndicatorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class HistoryCoinController {
+public class IndicatorController {
 
-    private final HistoryCoinService historyCoinService;
+    private final IndicatorService indicatorService;
 
-    @GetMapping("/api/historyCoins")
-    public ResponseEntity<ApiResponse> find100HistoryCoins() {
+    @GetMapping("/api/indicators")
+    public ResponseEntity<ApiResponse> find100Indicators() {
         try {
-            return ResponseMessageUtil.successMessage(historyCoinService.find100HistoryCoins());
+            return ResponseMessageUtil.successMessage(indicatorService.find100Indicators());
         } catch (CustomException ce) {
-            return ResponseMessageUtil.errorMessage(ce.getCode());
+            return ResponseMessageUtil.errorMessage(ce);
         } catch (Exception e) {
             return ResponseMessageUtil.errorMessage(e);
         }
